fb546e62b120df5555edc4a12cadc218f0daca27
jorgealbuermep <jorgealbuerme@gmail.com>
2022-07-14 14:06:01 -0400

    creando estructura del proyecto

---
+++ Models/Tarea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace taskList.Models
+{
+    public partial class Tarea
+    {
+        public int IdTarea { get; set; }
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+    }
+}
